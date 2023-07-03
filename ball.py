996324b3a32cf918f5404e2af83e137968a2cea9
nyk <nykcasseano13@gmail.com>
2023-07-03 11:49:00 -0300

    último commit

--- ball.py
+++ ball.py
@@ -16,6 +16,9 @@ class Ball(Turtle):
         new_y = self.ycor() + 10
         self.goto(new_x, new_y)
         
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
+    
+    def bounce_x(self):
+        self.x_move *= -1
         
--- main.py
+++ main.py
@@ -35,7 +35,10 @@ while game_is_on:
     
     #Detect the colision with wall 
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+    # Detect collision with  both paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340 :
+        ball.bounce_x()
     
     
 screen.exitonclick()
