2795cc93bd5692129f2616c14328147a0245fd69
nyk <nykcasseano13@gmail.com>
2023-07-03 10:45:47 -0300

    último commit

--- main.py
+++ main.py
@@ -4,7 +4,33 @@ Screen = Screen()
 screen.bgcolor = ("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+scree.tracer(0)
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+    
+    
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+    
+screen.listen()
+scree.onkey(go_up, "Up")
+scree.onkey(go_down, "Down")
 
 
-
+game_is_on = True
+while game_is_on:
+    screen.update()
+    
+    
 screen.exitonclick()
